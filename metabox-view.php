bbd8ca4f73310fd8e0e0b7872a6a002f4a09bec7
Peter Schilling <peter@peterschilling.org>
2013-01-28 17:29:49 -0800

    ui changes

--- metabox-view.php
+++ metabox-view.php
@@ -10,35 +10,56 @@ wp_enqueue_script(
 	array('jquery')
 );
 
+$state = get_post_meta( $post->ID, '_scroll_state', true );
+
+$copy = array();
+
+switch($state){
+	case 'active':
+		$copy['heading'] = "This Post is a Scroll";
+		break;
+	case 'inactive':
+		$copy['heading'] = "This Post has an Inactive Scroll";
+		$copy['activate'] = "Activate Scroll";
+		break;
+	default:
+		$copy['heading'] = "Convert this Post into a Scroll";
+		$copy['activate'] = "Convert Post";
+
+}
+
 ?>
 
 	<a href="#TB_inline?height=300&amp;width=400&amp;inlineId=scrollkit-post-options"
 		title="Scrollkit Post Settings"
 		class="thickbox"
-		id="scrollkit-post-options-trigger"
-		>Edit this post with Scroll Kit</a>
+		id="scrollkit-post-options-trigger">
+			Scroll Options
+	</a>
 
 	<div id="scrollkit-post-options" style="display:none">
-		<h2>This Post is Now a Scroll</h2>
+		<h2><?php echo $copy['heading'] ?></h2>
 
-		<?php echo get_post_meta( $post->ID, '_scroll_state', true ); ?>
 		<?php if (!empty($scrollkit_id)): ?>
-			<a href="<?php echo $this->build_scrollkit_edit_url($scrollkit_id) ?>"
-					target="_blank">
-				Edit this Scroll
+			<a href="<?php echo $this->build_scrollkit_edit_url($scrollkit_id) ?>" target="_blank">
+				Edit Scroll
 			</a>
 		<?php endif; ?>
 
+		<?php if( $state !== 'active' ): ?>
 		<br>
 		<a href="<?php bloginfo('url') ?>/?scrollkit=activate&p=<?php echo $post->ID ?>">
-			Convert to Scroll or Activate Scroll
+			<?php echo $copy['activate'] ?>
 		</a>
+		<?php endif ?>
 
+		<?php if( $state !== 'inactive' ): ?>
 		<br>
 		<a href="<?php bloginfo('url') ?>/?scrollkit=deactivate&p=<?php echo $post->ID ?>"
 				title="Turn this back into a normal wordpress post">
 			Dectivate Scroll
 		</a>
+		<?php endif ?>
 
 		<br>
 		<a href="<?php bloginfo('url') ?>/?scrollkit=delete&p=<?php echo $post->ID ?>"
@@ -46,6 +67,17 @@ wp_enqueue_script(
 				title="Permanently deletes the scroll associated with this post">
 			Delete Scroll
 		</a>
+
+<?php if (WP_DEBUG === true): ?>
+		<pre>
+DEBUG
+
+_scroll_id: <?php echo get_post_meta( $post->ID, '_scroll_id', true ); ?>
+
+_scroll_state: <?php echo get_post_meta( $post->ID, '_scroll_state', true ); ?>
+
+		</pre>
+<?php endif ?>
 
 	</div>
 
