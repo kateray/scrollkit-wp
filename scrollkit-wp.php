9a95ae89ee97c372e5d3ede92462a3b6089c008f
Peter Schilling <peter@peterschilling.org>
2013-06-01 14:52:17 -0400

    notify admin if api key isn't set. fixes #9

--- scrollkit-wp.php
+++ scrollkit-wp.php
@@ -31,6 +31,7 @@ class ScrollKit {
 		add_filter( 'template_redirect'    , array( $this, 'filter_template_redirect' ) );
 		add_filter( 'admin_footer'         , array( $this, 'filter_admin_footer') );
 		add_filter( 'plugin_action_links'  , array( $this, 'filter_plugin_action_links' ), 10, 2 );
+		add_filter( 'admin_footer'         , array( $this, 'warning' ) );
 
 		register_uninstall_hook( __FILE__  , array( 'Scroll', 'hook_delete_plugin_options' ) );
 	}
@@ -130,12 +131,22 @@ class ScrollKit {
 		if ( $file == plugin_basename( __FILE__ ) ) {
 			$settings_link = '<a href="' . menu_page_url( 'scroll-kit', false ) . '">' . __('Settings') . '</a>';
 
+
 			// make the 'Settings' link appear first
 			array_unshift( $links, $settings_link );
 		}
 
 		return $links;
 	}
+
+	function warning() {
+		$options = get_option( 'scroll_wp_options', self::option_defaults() );
+
+		if ( empty( $options['scrollkit_api_key'] ) ) {
+			echo '<div id="message" class="error"><p><strong>Scroll Kit is not active.</strong> You must <a href="' . menu_page_url( 'scroll-kit', false ) . '">add an API key</a> before it can work.</p></div>';
+		}
+	} // end warning()
+
 
 	/**
 	 * Delete options table entries when plugin deactivated AND deleted
